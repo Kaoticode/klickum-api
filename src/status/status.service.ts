60a11a64946e0e0e7c15112542f92397208c00d5
santiagoTrivi <santiagocarvajal103@gmail.com>
2024-11-15 03:22:47 -0400

    feat: cancell processes

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -1,16 +1,34 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { StatusRepository } from './status.repository';
-import { StatusType } from './domain/status.enum';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { StatusRepository } from "./status.repository";
+import { StatusEnum, StatusType } from "./domain/status.enum";
+import { Product } from "../product/model/product.entity";
 
 @Injectable()
 export class StatusService {
-  constructor(private readonly statusRepository: StatusRepository) {}
+  constructor(private readonly statusRepository: StatusRepository) {
+  }
 
   async findOne(name: StatusType) {
     const status = await this.statusRepository.findOne(name);
 
-    if (!status) throw new BadRequestException('status not found');
+    if (!status) throw new BadRequestException("status not found");
 
     return status;
+  }
+
+  public async productCancellation(type: "outOfStock" | "discontinued"): Promise<Partial<Product>> {
+
+    if (type === StatusEnum.discontinued.valueOf()) {
+      return {
+        isActive: false,
+        status: await this.statusRepository.findOne("discontinued")
+      };
+    }
+    if (type === StatusEnum.outOfStock.valueOf()) {
+      return {
+        status: await this.statusRepository.findOne("outOfStock")
+      };
+    }
+
   }
 }
