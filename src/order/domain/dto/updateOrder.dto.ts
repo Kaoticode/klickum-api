bca2e35c04723891996b6488f79ec74b1bd58170
santiagoTrivi <santiagocarvajal103@gmail.com>
2025-09-27 21:34:54 -0400

    feat: update order status

--- src/order/domain/dto/updateOrder.dto.ts
+++ src/order/domain/dto/updateOrder.dto.ts
@@ -1,9 +1,19 @@
-import { IsBoolean, IsOptional } from 'class-validator';
+import { IsBoolean, IsIn, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  StatusOrderList,
+  StatusOrderType,
+  StatusType,
+} from '../../../status/domain/status.enum';
 
 export class UpdateOrderDto {
   @ApiProperty()
   @IsBoolean()
   @IsOptional()
   isSent?: boolean;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsIn(StatusOrderList)
+  status?: StatusOrderType;
 }
